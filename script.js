28244496ae8ab8b59841e092211754b16d83afb5
Matheus Cardoso <101215639+MathuCardoso@users.noreply.github.com>
2023-08-08 22:46:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,17 +9,6 @@ var texto = document.getElementById('texto');
 
         mudar.onclick = function(){
 
-
-            //É preciso clicar duas vezes para aparecer a imagem (isso não foi proposital.)
-        if(titulo.textContent === 'NEYMAR CONFIRMADO NO PAYSANDU.'){
-            
-        var imgElement = document.createElement("img");
-
-        imgElement.src = "https://www.oliberal.com/image/contentid/policy:1.446688:1634211612/Paysandu.jpg";
-
-        document.body.appendChild(imgElement);
-        } else
-
         titulo.textContent = texto.value;
 
     }
@@ -32,10 +21,10 @@ var texto = document.getElementById('texto');
     }
 
     gf.onmouseleave = function(){
-        gfText.textContent = 'Não vá! Eu sou carente';
+        gfText.textContent = 'Por favor, não vá.';
     }
 
-    /*/ 
+    /*/ Não estava funcionando corretamente
     gf.onmousemove = function(){
         gfText.textContent = '';
     }
@@ -44,14 +33,90 @@ var texto = document.getElementById('texto');
     
 
     gf.onclick = function(){
-        gfText.textContent = 'Ei, me deixa quieto!';
+        gfText.textContent = 'HAHAHA isso faz cócegas';
     }
 
-    var sorveteText = document.getElementById('sorveteText').value;
-    var sorveteResposta = document.getElementById('sorveteResposta');
+    
 
-    if(sorveteText.length > 3){
-        if(sorveteText == 'chocolate'){
-            sorveteResposta.textContent = ''
+    sorveteBtn.onclick = function(){
+
+        var sorveteText = document.getElementById('sorveteText').value;
+        var sorveteResposta = document.getElementById('sorveteResposta');
+        var sorveteBtn = document.getElementById('sorveteBtn');
+
+        if(sorveteText.length > 3){
+
+            if(sorveteText == 'chocolate'){
+                sorveteResposta.textContent = 'Jeff: Eu amo sorvete de chocolate!';
+            } 
+            else if(sorveteText == 'flocos'){
+                sorveteResposta.textContent = 'Jeff: Ahh, mas chocolate é o meu favorito...';
+            }
+            else
+            sorveteResposta.textContent = 'Jeff: Ahh, eu esperava um sabor de sorvete...';
+
+
         }
+        else if(sorveteText == ''){
+            sorveteResposta.textContent = 'Você não respondeu a pergunta!';
+        } 
+        else
+        sorveteResposta.textContent = 'Jeff: Ahh, eu esperava um sabor de sorvete...';
     }
+
+    //Exercício 6
+
+    var dlMode = document.getElementById('DlMode');
+
+    dlMode.onclick = function(){
+
+        if(dlMode.innerHTML === 'Modo Escuro'){
+        var body = document.getElementById('body');
+        body.style.backgroundColor = 'black';
+        body.style.color = 'white';
+        dlMode.innerHTML = 'Modo Claro'
+    } 
+    else if(dlMode.innerHTML === 'Modo Claro'){
+        var body = document.getElementById('body');
+        body.style.backgroundColor = 'white';
+        body.style.color = 'black';
+        dlMode.innerHTML = 'Modo Escuro'
+    }
+    }
+
+
+
+    var enviar = document.getElementById('submit');
+
+    enviar.onclick = function(){
+
+    var name = document.getElementById('name').value;
+    var senha = document.getElementById('senha').value;
+    var senhaConf = document.getElementById('senhaConf').value;
+    var msgErro = document.getElementById('msgErro');
+    var msgSucesso = document.getElementById('msgSucesso');
+    var reset = document.getElementById('reset');
+
+    if(!name){
+        msgErro.textContent = 'Preencha o nome.';
+    }
+    else if(!senha){
+        msgErro.textContent = 'Preencha a senha.';
+    }
+    else if(senha.length < 8){
+        msgErro.textContent = 'A senha não pode ter menos que 8 caracteres.';
+    }
+    else if(!senhaConf){
+        msgErro.textContent = 'Confirme a senha.';
+    }
+    else if(senha != senhaConf){
+        msgErro.textContent = 'As senhas devem ser idênticas.'
+    } 
+    else{
+    msgSucesso.textContent = 'Cadastro realizado com sucesso!';
+    msgErro.textContent = '';
+    reset.click();
+}
+}
+
+    
